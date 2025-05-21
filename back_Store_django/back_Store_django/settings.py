464250e60be9c17a982c41b9cdb6fa6072fefe41
xyz-ai <73694635+xyz-ai@users.noreply.github.com>
2025-05-21 10:21:39 +0800

    Update settings.py

--- back_Store_django/back_Store_django/settings.py
+++ back_Store_django/back_Store_django/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 # Media files
-MEDIA_URL = 'https://store-city.onrender.com/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
