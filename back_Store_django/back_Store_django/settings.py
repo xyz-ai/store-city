9b0bf5aa54a81857a53c0df451a21f97be945a63
xyz-ai <73694635+xyz-ai@users.noreply.github.com>
2025-05-21 09:50:00 +0800

    Update settings.py

--- back_Store_django/back_Store_django/settings.py
+++ back_Store_django/back_Store_django/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 ALLOWED_HOSTS = ['*.onrender.com', 'localhost', '127.0.0.1', 'store-city.onrender.com']
 
 
