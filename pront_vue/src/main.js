b2e7800e0cf3bc50a102b0234ffb23fc882db732
xyz-ai <73694635+xyz-ai@users.noreply.github.com>
2025-05-21 10:23:48 +0800

    Update main.js

--- pront_vue/src/main.js
+++ pront_vue/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://store-city.onrender.com'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 createApp(App).use(store).use(router, axios).mount('#app')
 store.dispatch('initializeCart');
