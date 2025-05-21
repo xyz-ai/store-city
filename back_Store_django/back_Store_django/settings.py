d25a5bae4c723915fd95dfedb8eafbe25cb61b89
xyz-ai <73694635+xyz-ai@users.noreply.github.com>
2025-05-21 10:00:57 +0800

    Update settings.py

--- back_Store_django/back_Store_django/settings.py
+++ back_Store_django/back_Store_django/settings.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
